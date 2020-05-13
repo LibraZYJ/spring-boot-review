870fc25306ac9bc0300f30954256d46da12663a6
zyj <1836686674@qq.com>
2020-05-13 16:45:41 +0800

    单数据源JPA

--- spring-boot-jpa/src/main/java/com/soft1851/springboot/jpa/dao/UserRepository.java
+++ spring-boot-jpa/src/main/java/com/soft1851/springboot/jpa/dao/UserRepository.java
@@ -1,8 +1,13 @@
 package com.soft1851.springboot.jpa.dao;
 
-import com.soft1851.springboot.jpa.model.cascade.User;
+import com.soft1851.springboot.jpa.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 /**
  * @author Yujie_Zhao
  * @ClassName UserRepository
@@ -10,9 +15,73 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @Date 2020/5/13  8:43
  * @Version 1.0
  **/
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByUserName(String userName);
+public interface UserRepository extends JpaRepository<User, Long> {
 
 
-    User findByUserNameOrEmail(String username,String email);
+    /**
+     * 根据方法名解析：按照age查询所有
+     * @param age
+     * @return
+     */
+    User findAllByAgeBefore(int age);
+
+    /**
+     * 根据方法名解析：按userName和password相等查询唯一记录
+     *
+     * @param userName
+     * @param password
+     * @return
+     */
+    User findByUserNameEqualsAndPasswordEquals(String userName, String password);
+
+    /**
+     * 根据方法名解析：按nickName模糊查询一组记录
+     *
+     * @param nickName
+     * @return
+     */
+    List<User> findUsersByNickNameLike(String nickName);
+
+    /**
+     * 根据方法名解析：查询年龄大于指定age的所有用户
+     *
+     * @param age
+     * @return
+     */
+    List<User> findUsersByAgeGreaterThan(int age);
+
+    /**
+     * 自定义JPQL查询，类似 Hibernate的 HQL语法，在接口上使用 @Query
+     *
+     * @param id
+     * @return
+     */
+    @Query("select u from User u where u.id = ?1")
+    User findById(long id);
+
+    /**
+     * 按id修改nickName
+     *
+     * @param nickName
+     * @param id
+     * @return
+     */
+    @Modifying
+    @Transactional(rollbackFor = RuntimeException.class)
+    @Query(value = "update user set nick_name = ?1 where id = ?2", nativeQuery = true)
+    int updateNickName(String nickName, long id);
+
+
+    /**
+     * 插入
+     *
+     * @param userName
+     * @param password
+     * @param email
+     * @return
+     */
+    @Transactional(rollbackFor = RuntimeException.class)
+    @Modifying
+    @Query(value = "insert into user(user_name, password,email) values (:userName, :password,:email)", nativeQuery = true)
+    int insertUser(@Param("userName") String userName, @Param("password") String password, @Param("email") String email);
 }
--- spring-boot-jpa/src/test/java/com/soft1851/springboot/jpa/dao/UserRepositoryTest.java
+++ spring-boot-jpa/src/test/java/com/soft1851/springboot/jpa/dao/UserRepositoryTest.java
@@ -1,15 +1,23 @@
 package com.soft1851.springboot.jpa.dao;
 
-import com.soft1851.springboot.jpa.model.cascade.User;
+import com.soft1851.springboot.jpa.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @Slf4j
@@ -17,26 +25,124 @@ class UserRepositoryTest {
     @Resource
     private UserRepository userRepository;
 
+    @Test
+    void findByUserNameEqualsAndPasswordEquals() {
+        User user = userRepository.findByUserNameEqualsAndPasswordEquals("user3","123456");
+        System.out.println(user);
+        assertNotNull(userRepository.findByUserNameEqualsAndPasswordEquals("user3","123456"));
+    }
 
     @Test
-    public void testSave() {
+    void findUsersByNickNameLike() {
+        //昵称中含有“nick”的人数
+//        assertEquals(18, userRepository.findUsersByNickNameLike("%nick%").size());
+        List<User> users = userRepository.findUsersByNickNameLike("%nick%");
+        System.out.println(users);
+    }
 
-        List<User> users = new ArrayList<>(Arrays.asList(
-                User.builder().userName("zyj").passWord("123").nickName("zyj").email("1836686674@qq.com").build())
-                );
-        // 保存多个
+    @Test
+    void findUsersByAgeGreaterThan() {
+        //查询年龄大于20的所有用户
+        log.info(String.valueOf(userRepository.findUsersByAgeGreaterThan(20).size()));
+    }
+
+    @Test
+    void findById() {
+        //自定义的JPQL查询，根据id查询
+        assertEquals("aaa", userRepository.findById(1L).getUserName());
+    }
+
+    @Test
+    void updateNickName() {
+        //使用save活saveAndFlush方法更新数据，必须要提供所有字段值，否则该字段为空
+        User user = User.builder()
+                .id(1L)
+                .userName("修改名称")
+                .email("123456789@qq.com")
+                .password("修改密码")
+                .nickName("修改昵称")
+                .age(12)
+                .regTime(LocalDateTime.now()).build();
+        userRepository.saveAndFlush(user);
+        //可以自定义SQL实现更新
+        int n = userRepository.updateNickName("新的昵称", 3L);
+        assertEquals(1, n);
+    }
+
+    @Test
+    void insertUser() {
+        //自定义插入
+        int n = userRepository.insertUser("zyj", "zyj666", "1836686674@qq.com");
+        assertEquals(1, n);
+    }
+
+    @Test
+    void findAllByAgeBefore() {
+        User user =userRepository.findAllByAgeBefore(20);
+        System.out.println(user);
+    }
+
+
+    @Test
+    void testSave() {
+        User user = User.builder()
+                .userName("zyj")
+                .email("1836686674@qq.com")
+                .password("123123")
+                .nickName("Libra")
+                .age(11)
+                .regTime(LocalDateTime.now()).build();
+        userRepository.save(user);
+
+//        批量加入
+        List<User> users = new ArrayList<>();
+        for (int i = 1; i <= 20; i++) {
+            User testUser = User.builder()
+                    .userName("zyj" + i)
+                    .email("zyj" + i + "@qq.com")
+                    .password("123456")
+                    .nickName("nickName" + i)
+                    .age(new Random().nextInt(50))
+                    .regTime(LocalDateTime.now()).build();
+            users.add(testUser);
+        }
         userRepository.saveAll(users);
+        assertEquals(21, userRepository.findAll().size());
     }
+
+
     @Test
-    void findByUserName() {
-        User user = userRepository.findByUserName("zyj");
-        log.info(String.valueOf(user));
+    void testDelete() {
+        //自带删除方法
+        userRepository.deleteById(3L);
     }
 
     @Test
-    void findByUserNameOrEmail() {
-        User user = userRepository.findByUserNameOrEmail("zyj","1836686674@qq.com");
-        log.info(String.valueOf(user));
+    void testSelect() {
+        // 查询所有
+        userRepository.findAll().forEach(user -> log.info(user.toString()));
+        // 分页查询全部，返回封装了的分页信息， jpa页码从0开始
+        Page<User> pageInfo = userRepository.findAll(
+                PageRequest.of(1, 3, Sort.Direction.ASC, "id"));
+        log.info("总记录数： {}", pageInfo.getTotalElements());
+        log.info("当前页记录数： {}", pageInfo.getNumberOfElements());
+        log.info("每页记录数： {}", pageInfo.getSize());
+        log.info("获取总页数： {}", pageInfo.getTotalPages());
+        log.info("查询结果： {}", pageInfo.getContent());
+        log.info("当前页（从0开始计）： {}", pageInfo.getNumber());
+        log.info("是否为首页： {}", pageInfo.isFirst());
+        log.info("是否为尾页： {}", pageInfo.isLast());
+        // 条件查询
+        User user = User.builder().userName("user").build();
+        List<User> users = userRepository.findAll(Example.of(user));
+        log.info("满足条件的记录有：");
+        users.forEach(user1 -> log.info(user1.toString()));
+        // 单个查询
+        User user1 = User.builder().id(2L).build();
+        Optional<User> optionalUser = userRepository.findOne(Example.of(user1));
+        log.info("单个查询结果： {}", optionalUser.orElse(null));
     }
+
+
 
 }
